84f09fc756183e2dde88d62dd3f315d8e3d529aa
StudioN2025 <abobachebaksar@gmail.com>
2026-04-11 17:31:15 +0300

    Update svo.js

--- api/modules/svo.js
+++ api/modules/svo.js
@@ -1,7 +1,7 @@
 const { sendMessage, saveData, addCapturedBasement, removeCapturedBasement } = require('./helpers');
 const config = require('./config');
 
-async function handleSvoCommand(cleanText, rawText, user, data, BOT_TOKEN, chatId, username) {
+async function handleSvoCommand(cleanText, rawText, user, data, BOT_TOKEN, chatId, username, userId) {
   const nowTime = Date.now();
   
   if (cleanText === '/svo') {
